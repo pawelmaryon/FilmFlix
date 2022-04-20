0efb6e72bac829117c5f29a21f65080534af3765
P Maryon <maryonpawel@gmail.com>
2022-04-20 11:01:09 +0100

    find by year functionality applied

--- readGenreRecords.py
+++ readGenreRecords.py
@@ -1,9 +1,10 @@
 from dbConnect import *
-
+import time
 def readGenreRecords():
   listOfGenre = ["Comedy", "Test", "Action", "Crime", "Animation", "Fantasy", "Fighting"]
   print("Full list of film genre available:")
   for genre in listOfGenre:
+    time.sleep(0.5)
     print(genre)
   genreType = input("What type of film you are looking for? ").title()
   genreType = "'" + genreType + "'"
@@ -12,6 +13,7 @@ def readGenreRecords():
     cursor.fetchall()
   )
   for record in row:
+    time.sleep(1)
     print(record)
 
 readGenreRecords()
