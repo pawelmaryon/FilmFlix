57f02a644c9629682d4d248b9d5d005ea1a9f4b3
P Maryon <maryonpawel@gmail.com>
2022-04-20 12:13:08 +0100

    reading rating functionality done

--- readRatingRecords.py
+++ readRatingRecords.py
@@ -2,4 +2,22 @@ from dbConnect import *
 import time
 
 def readRatingRecords():
-  print("reading")
+  print("What rating you are looking for?")
+  print("Pick one or more from list below")
+  cursor.execute(f"SELECT DISTINCT rating FROM tblFilms")
+  ratingList = (
+    cursor.fetchall()
+  )
+
+  for rating in ratingList:
+    print(rating)
+  ratingRercord = input("Which rating would you like to see? ").upper()
+  ratingRercord = "'" + ratingRercord + "'"
+  cursor.execute(f"SELECT * FROM tblFilms WHERE rating={ratingRercord}")
+  row = (
+    cursor.fetchall()
+  )
+  for rating in row:
+    time.sleep(0.5)
+    print(rating)
+readRatingRecords()
